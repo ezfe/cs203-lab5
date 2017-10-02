ee3bf176014d42e6173ff075ad348b43aad61295
Ezekiel Elin <ezekielelin@me.com>
2017-10-02 14:20:48 -0400

    initial commit

---
+++ decl.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define TMin INT_MIN
+#define TMax INT_MAX
+
+#include "btest.h"
+#include "bits.h"
+
+test_rec test_set[] = {
+/* Copyright (C) 1991-2017 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+/* This header is separate from features.h so that the compiler can
+   include it implicitly at the start of every compilation.  It must
+   not itself include <features.h> or any other header that includes
+   <features.h> because the implicit include comes before any feature
+   test macros that may be defined in a source file before it first
+   explicitly includes a system header.  GCC knows the name of this
+   header in order to preinclude it.  */
+/* glibc's intent is to support the IEC 559 math functionality, real
+   and complex.  If the GCC (4.9 and later) predefined macros
+   specifying compiler intent are available, use them to determine
+   whether the overall intent is to support these features; otherwise,
+   presume an older compiler has intent to support these features and
+   define these macros by default.  */
+/* wchar_t uses Unicode 9.0.0.  Version 9.0 of the Unicode Standard is
+   synchronized with ISO/IEC 10646:2014, fourth edition, plus
+   Amd. 1  and Amd. 2 and 273 characters from forthcoming  10646, fifth edition.
+   (Amd. 2 was published 2016-05-01,
+   see https://www.iso.org/obp/ui/#iso:std:iso-iec:10646:ed-4:v1:amd:2:v1:en) */
+/* We do not support C11 <threads.h>.  */
+ {"bitCount", (funct_t) bitCount, (funct_t) test_bitCount, 1, "! ~ & ^ | + << >>", 40, 4,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"bitXor", (funct_t) bitXor, (funct_t) test_bitXor, 2, "& ~", 14, 1,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+{"byteSwap", (funct_t) byteSwap, (funct_t) test_byteSwap, 3,
+     "! ~ & ^ | + << >>", 25, 2,
+    {{TMin, TMax},{0,3},{0,3}}},
+ {"isPower2", (funct_t) isPower2, (funct_t) test_isPower2, 1, "! ~ & ^ | + << >>", 20, 4,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"logicalNeg", (funct_t) logicalNeg, (funct_t) test_logicalNeg, 1,
+    "~ & ^ | + << >>", 12, 4,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"upperBits", (funct_t) upperBits, (funct_t) test_upperBits, 1, "! ~ & ^ | + << >>", 10, 1,
+  {{0, 32},{TMin,TMax},{TMin,TMax}}},
+ {"minusOne", (funct_t) minusOne, (funct_t) test_minusOne, 0,
+    "! ~ & ^ | + << >>", 2, 1,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"isGreater", (funct_t) isGreater, (funct_t) test_isGreater, 2,
+    "! ~ & ^ | + << >>", 24, 3,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"bitParity", (funct_t) bitParity, (funct_t) test_bitParity, 1, "! ~ & ^ | + << >>", 20, 4,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+  {"", NULL, NULL, 0, "", 0, 0,
+   {{0, 0},{0,0},{0,0}}}
+};
---
+++ tests.c
@@ -0,0 +1,151 @@
+/* Testing Code */
+
+#include <limits.h>
+#include <math.h>
+
+/* Routines used by floation point test code */
+
+/* Convert from bit level representation to floating point number */
+float u2f(unsigned u) {
+  union {
+    unsigned u;
+    float f;
+  } a;
+  a.u = u;
+  return a.f;
+}
+
+/* Convert from floating point number to bit-level representation */
+unsigned f2u(float f) {
+  union {
+    unsigned u;
+    float f;
+  } a;
+  a.f = f;
+  return a.u;
+}
+
+/* Copyright (C) 1991-2017 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+/* This header is separate from features.h so that the compiler can
+   include it implicitly at the start of every compilation.  It must
+   not itself include <features.h> or any other header that includes
+   <features.h> because the implicit include comes before any feature
+   test macros that may be defined in a source file before it first
+   explicitly includes a system header.  GCC knows the name of this
+   header in order to preinclude it.  */
+/* glibc's intent is to support the IEC 559 math functionality, real
+   and complex.  If the GCC (4.9 and later) predefined macros
+   specifying compiler intent are available, use them to determine
+   whether the overall intent is to support these features; otherwise,
+   presume an older compiler has intent to support these features and
+   define these macros by default.  */
+/* wchar_t uses Unicode 9.0.0.  Version 9.0 of the Unicode Standard is
+   synchronized with ISO/IEC 10646:2014, fourth edition, plus
+   Amd. 1  and Amd. 2 and 273 characters from forthcoming  10646, fifth edition.
+   (Amd. 2 was published 2016-05-01,
+   see https://www.iso.org/obp/ui/#iso:std:iso-iec:10646:ed-4:v1:amd:2:v1:en) */
+/* We do not support C11 <threads.h>.  */
+int test_bitCount(int x) {
+  int result = 0;
+  int i;
+  for (i = 0; i < 32; i++)
+    result += (x >> i) & 0x1;
+  return result;
+}
+int test_bitXor(int x, int y)
+{
+  return x^y;
+}
+int test_byteSwap(int x, int n, int m)
+{
+    /* little endiamachine */
+    /* least significant byte stored first */
+    unsigned int nmask, mmask;
+    switch(n) {
+    case 0:
+      nmask = x & 0xFF;
+      x &= 0xFFFFFF00;
+      break;
+    case 1:
+      nmask = (x & 0xFF00) >> 8;
+      x &= 0xFFFF00FF;
+      break;
+    case 2:
+      nmask = (x & 0xFF0000) >> 16;
+      x &= 0xFF00FFFF;
+      break;
+    default:
+      nmask = ((unsigned int)(x & 0xFF000000)) >> 24;
+      x &= 0x00FFFFFF;
+      break;
+    }
+    switch(m) {
+    case 0:
+      mmask = x & 0xFF;
+      x &= 0xFFFFFF00;
+      break;
+    case 1:
+      mmask = (x & 0xFF00) >> 8;
+      x &= 0xFFFF00FF;
+      break;
+    case 2:
+      mmask = (x & 0xFF0000) >> 16;
+      x &= 0xFF00FFFF;
+      break;
+    default:
+      mmask = ((unsigned int)(x & 0xFF000000)) >> 24;
+      x &= 0x00FFFFFF;
+      break;
+    }
+    nmask <<= 8*m;
+    mmask <<= 8*n;
+    return x | nmask | mmask;
+}
+int test_isPower2(int x) {
+  int i;
+  for (i = 0; i < 31; i++) {
+    if (x == 1<<i)
+      return 1;
+  }
+  return 0;
+}
+int test_logicalNeg(int x)
+{
+  return !x;
+}
+int test_upperBits(int x) {
+  int result = 0;
+  int i;
+  for (i = 0; i < x; i++)
+    result |= (1 << (31-i));
+  return result;
+}
+int test_minusOne(void) {
+  return -1;
+}
+int test_isGreater(int x, int y)
+{
+  return x > y;
+}
+int test_bitParity(int x) {
+  int result = 0;
+  int i;
+  for (i = 0; i < 32; i++)
+    result ^= (x >> i) & 0x1;
+  return result;
+}
